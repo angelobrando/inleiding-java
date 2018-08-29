59c893a9070504670e9a39314e095a59faeb1306
angelobrando <2064763@talnet.nl>
2018-08-29 13:44:29 +0200

    fuck java en de desktop env.

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -2,10 +2,10 @@ package h01;
 
 /**
  * Created by piet on 31-08-16.
- */
+         */
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo klas 7A!");
+        System.out.println("Hallo roc-dev!");
     }
 }
