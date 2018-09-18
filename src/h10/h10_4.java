4e8ca35d6982450300c6ba41e77cb06986e3cc26
angelobrando <42804168+angelobrando@users.noreply.github.com>
2018-09-19 00:22:26 +0200

    Update h10_4.java

--- src/h10/h10_4.java
+++ src/h10/h10_4.java
@@ -2,28 +2,33 @@ package h10;
 import java.awt.*;
 import java.applet.Applet;
 import java.awt.event.*;
-public class h10_4 extends Applet {
-    double doub1;
-    double doub2;
+public class if10_4 extends Applet {
     TextField tv;
     Button knop;
     int in1;
     String string1;
     String string2;
     TextField tv2;
+    int in2;
+    String string3;
     public void init(){
         knop = new Button("knop");
         tv = new TextField(20);
         knop.addActionListener(new knoplistener());
+        tv2 = new TextField("jaar",20);
         add (knop);
         add (tv);
+        add (tv2);
         in1 = 0;
         string1 = "";
         string2 = "";
-        tv2 = new TextField();
+
+        string3 = "";
+        in2 = 0;
     }
     public void paint (Graphics g){
         g.drawString(string2,40,40);
+        g.drawString(string3,60,40);
 
     }
     private class knoplistener implements ActionListener{
@@ -31,12 +36,19 @@ public class h10_4 extends Applet {
         public void actionPerformed (ActionEvent e){
             string1 = tv.getText();
             in1 = Integer.parseInt(string1);
+            string3 = tv2.getText();
+            in2 = Integer.parseInt(string3);
             switch (in1){
                 case 1:
                     string2 = "31";
                     break;
                 case 2:
-                    string2 = "28/29";
+                    if (in2 % 4 == 0){
+                        string2 = "28";
+                    }
+                    else {
+                        string2 = "29";
+                    }
                     break;
                 case 3:
                     string2 = "31";
@@ -66,7 +78,7 @@ public class h10_4 extends Applet {
                     string2 = "30";
                     break;
                 case 12:
-                    string2 = "3";
+                    string2 = "31";
                     break;
                 default:
                     string2 = "Die maan kennen wij nog niet";
