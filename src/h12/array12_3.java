0ce0e93377ba92c955b6daeb64605e99a3cc5eab
angelobrando <42804168+angelobrando@users.noreply.github.com>
2018-11-19 18:53:00 +0100

    >?#190:}Juli@+++

--- src/h12/array12_3.java
+++ src/h12/array12_3.java
@@ -2,13 +2,43 @@ package h12;
 
 import java.applet.Applet;
 import java.awt.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
 public class array12_3 extends Applet {
 
+TextField[] tv;
+Button okknop;
+int[] intteller;
+String[] stringtemp;
     public void init(){
-
+        setSize(6000,500);
+        tv = new TextField[5];
+        okknop = new Button("Ok-Knop");
+        okknop.addActionListener(new KnopActionListener());
+        add(okknop);
+        intteller = new int[5];
+        stringtemp = new String[5];
+            for (int teller = 0; teller < stringtemp.length; teller ++){
+                tv[teller] = new TextField(20);
+                add(tv[teller]);
+        }
     }
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
+        for (int teller = 0; teller < tv.length; teller++) {
+            Arrays.sort(intteller);
+            g.drawString("" + intteller[teller], 200, 30 * teller + 20);
+        }
+    }
+     private class KnopActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            for (int teller = 0; teller < tv.length; teller ++){
+                stringtemp[teller] = tv[teller].getText();
+                intteller[teller] = Integer.parseInt(stringtemp[teller]);
+                repaint();
 
+            }
+        }
     }
 }
